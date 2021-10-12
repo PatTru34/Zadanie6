278ddd8de4edb3c28b920ef2e8cd5afec3a67fe8
Patryk Truchan <162391@student.uwm.edu.pl>
2021-10-12 09:23:23 +0200

    Initial commit

---
+++ src/pl/edu/uwm/wmii/kotewa/laboratorium00/Zadanie6.java
@@ -0,0 +1,15 @@
+package pl.edu.uwm.wmii.kotewa.laboratorium00;
+
+public class Zadanie6 {
+
+    public static void main(String[] args) {
+        System .out. println ("     ///    ");
+        System .out. println (" ['''''''''] ");
+        System .out. println (" (| ^   ^ |) ");
+        System .out. println ("  |   $   |  ");
+        System .out. println ("   |  V  | ");
+        System .out. println ("    |   | ");
+        System .out. println ("     | | ");
+        System .out. println ("      | ");
+    }
+}
